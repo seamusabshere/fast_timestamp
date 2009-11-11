2ee043bb0ed2c5884db7551ad89e0ca903d1fbbd
Seamus Abshere <seamus@abshere.net>
2009-11-11 06:40:35 -0500

    Provide #set_timestamp!(key, time)

--- lib/fast_timestamp.rb
+++ lib/fast_timestamp.rb
@@ -5,11 +5,7 @@ module FastTimestamp
   end
   
   def timestamp!(key)
-    raise "Can't timestamp new records" if new_record?
-    transaction do
-      t = ::Timestamp.find_or_create_by_timestampable_type_and_timestampable_id_and_key(self.class.base_class.name, id, key.to_s)
-      t.update_attribute :stamped_at, Time.zone.now
-    end
+    set_timestamp! key, Time.zone.now
   end
   
   def untimestamp!(key)
@@ -27,6 +23,14 @@ module FastTimestamp
   def timestamp_for(key)
     if t = ::Timestamp.find_by_timestampable_type_and_timestampable_id_and_key(self.class.base_class.name, id, key.to_s)
       t.read_attribute :stamped_at
+    end
+  end
+  
+  def set_timestamp!(key, time)
+    raise "Can't timestamp new records" if new_record?
+    transaction do
+      t = ::Timestamp.find_or_create_by_timestampable_type_and_timestampable_id_and_key(self.class.base_class.name, id, key.to_s)
+      t.update_attribute :stamped_at, time
     end
   end
 
